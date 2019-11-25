d440723a78dcbb3e4a4d709aabeaf1535ecb9c73
Edgars Kuzmanis <edgars.kuzmanis@scandiweb.com>
2019-11-25 23:31:43 +0200

    Updates

---
+++ precache-manifest.708920c6f487acaf835f25af4d3b779a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ee2d850360a1cfea1e8cfe6c308e75",
+    "url": "/FigureDrawer/index.html"
+  },
+  {
+    "revision": "6f98c9c96acd72d83b37",
+    "url": "/FigureDrawer/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "159c8f1a4217ea6b9e44",
+    "url": "/FigureDrawer/static/js/2.ca88543b.chunk.js"
+  },
+  {
+    "revision": "6f98c9c96acd72d83b37",
+    "url": "/FigureDrawer/static/js/main.273ee6d6.chunk.js"
+  },
+  {
+    "revision": "10faf8ec4984398410fe",
+    "url": "/FigureDrawer/static/js/runtime-main.67d7b59c.js"
+  }
+]);
